922a4fb42cdccec6af9b778f3380bbc51244df1a
Charlie Tian <charlie.tian@gmail.com>
2014-03-10 18:50:34 -0400

    2

--- src/ch2.clj
+++ src/ch2.clj
@@ -20,5 +20,55 @@
 h
 ;= {[1 2] 3}
 
-;; A critial choice [ page 58 ]
+;; functions as data permits a means of abstraction that a language
+;; without first-class functions lacks.
+
+(defn call-twice [f x]
+  (f x)
+  (f x))
+
+(call-twice println 123)
+
+(max 5 6)
+;= 6
+
+(require 'clojure.string)
+(clojure.string/lower-case "Clojure")
+;= "clojure"
+
+;; Clojure functions are values themselves, they can be used with
+;; higher-order fuctions, which are functions that take other
+;; functions as arguments or return a function as a result.
+
+(map clojure.string/lower-case ["Java" "Imperative" "Weeping"
+                                "Clojure" "Learning" "Peace"])
+;= ("java" "imperative" "weeping" "clojure" "learning" "peace")
+(map * [1 2 3 4] [5 6 7 8])
+;= (5 12 21 32)
+
+(reduce max [0 -3 10 48])
+;= 48
+
+;; provide an initial value to "seed" the reduction
+(reduce + 50 [1 2 3 4])
+;= 60
+
+(reduce
+ (fn [m v]
+   (assoc m v (* v v)))
+ {}
+ [1 2 3 4])
+;= {4 16, 3 9, 2 4, 1 1}
+
+(reduce
+ #(assoc %1 %2 (* %2 %2))
+ {}
+ [1 2 3 4])
+;= {4 16, 3 9, 2 4, 1 1}
+
+;; Applying ourselves partially [ page 65 ]
+
+
+
+
 
